870f115777868ff2fd442c2be23bbb3735eb8b72
Torben <torben@zobra.com>
2021-03-12 01:22:14 +0000

    Random string

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
   <section>
     <div class="row at-row flex-center flex-middle">
       <div class="col-lg-24">
+				Random string here to test rebuild
         <a href="/"><img class="super-justice" :src="headerImage"></a>
       </div>
     </div>
